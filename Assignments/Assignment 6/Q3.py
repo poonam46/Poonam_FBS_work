ea60dda586f23e57a6e5df3b1e13c8499153221e
poonam46 <poonampawar462003@gmail.com>
2026-01-06 14:56:17 +0530

    Update Q3.py

--- Assignments/Assignment 6/Q3.py
+++ Assignments/Assignment 6/Q3.py
@@ -1,22 +1,10 @@
 for i in range(1,5):
+    k = 1
     for j in range(1,5-i):
-        print(" ", end = " ")
+      print(" ",end = " ")
 
     for j in range(1,i+1):
-        if j == 2 or j == 4 :
-            print(" ", end = " ")
-        elif(i == 3 and j == 3):
-            print(2 , end = " ")
-        else:
-            print(j , end = " ")
-
-    for j in range(i - 1 , 0 , -1):
-        if j == 2 :
-            print(" ", end = " ")
-        else:
-            print(j , end = " ")
-    
-    for j in range(1,5-i):
-        print(" " , end = " ")
+      print(k,end = " ")
+      k = k * (i - j) // j
     
     print()
